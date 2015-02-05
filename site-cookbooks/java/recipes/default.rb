a99112092b295cdc49287d176ffc94253f2544bf
hiroshi shimoda <hilolih@gmail.com>
2015-02-05 11:52:29 +0900

    add v1 server, change rbenv recipe, add java recipe

---
+++ site-cookbooks/java/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: java
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "java-1.7.0-openjdk" do
+  action :install
+end
+
+
--- site-cookbooks/rbenv/recipes/default.rb
+++ site-cookbooks/rbenv/recipes/default.rb
@@ -8,6 +8,12 @@
 #
 
 home = "/home/vagrant"
+RBENV_SCRIPT = "/etc/profile.d/rbenv.sh"
+
+# for 2.2.0
+package "libffi-devel" do
+  action :install
+end
 
 git "#{home}/.rbenv" do
   repository "https://github.com/sstephenson/rbenv.git"
@@ -17,20 +23,13 @@ git "#{home}/.rbenv" do
   action :sync
 end
 
-script "install_vagrant" do
-  interpreter "bash"
-  user "vagrant"
-  group "vagrant"
-  code <<-EOL
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-    echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-    mkdir -p ~/.rbenv/plugins
-    source ~/.bash_profile
-  EOL
-  environment 'HOME' => "/home/vagrant", "SHELL" => "/bin/bash"
-  not_if "grep rbenv /home/vagrant/.bash_profile"
+cookbook_file "rbenv.sh" do
+  mode 0644
+  owner "root"
+  group "root"
+  path RBENV_SCRIPT
+  action :create_if_missing
 end
-
 
 git "#{home}/.rbenv/plugins/ruby-build" do
   repository "https://github.com/sstephenson/ruby-build.git"
@@ -38,8 +37,26 @@ git "#{home}/.rbenv/plugins/ruby-build" do
   action :sync
 end
 
+#script "install_vagrant" do
+#  interpreter "bash"
+#  user "vagrant"
+#  group "vagrant"
+#  #code <<-EOL
+#  #  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+#  #  echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+#  #  mkdir -p ~/.rbenv/plugins
+#  #  source ~/.bash_profile
+#  #EOL
+#  environment 'HOME' => "/home/vagrant", "SHELL" => "/bin/bash"
+#  not_if "grep rbenv /home/vagrant/.bash_profile"
+#end
+
+
 %w{
-}.each do |version|
-  execute "install ruby #{version}" do
+  pry
+}.each do |gem|
+  execute "install ruby #{gem}" do
+    command "source #{RBENV_SCRIPT}; gem install #{gem}; rbenv rehash"
+    not_if "source #{RBENV_SCRIPT}; gem list | grep #{gem}"
   end
 end
