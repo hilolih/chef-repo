6134996423d1c245fdfc2c0a3af72b037d4f7124
hiroshi shimoda <hilolih@gmail.com>
2015-02-21 01:47:27 +0900

    install fluentd

--- site-cookbooks/fluentd/recipes/default.rb
+++ site-cookbooks/fluentd/recipes/default.rb
@@ -6,3 +6,29 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cookbook_file "sysctl.conf" do
+  mode 0644
+  owner "root"
+  group "root"
+  path "/etc/sysctl.conf"
+  not_if "grep tcp_tw_recycle /etc/sysctl.conf"
+end
+
+cookbook_file "limits.conf" do
+  mode 0644
+  owner "root"
+  group "root"
+  path "/etc/security/limits.conf"
+  not_if "grep tcp_tw_recycle /etc/security/limits.conf"
+end
+
+bash "install td-agent" do
+  code <<-EOL
+    curl -L http://toolbelt.treasuredata.com/sh/install-redhat.sh | sh 
+  EOL
+end
+
+service "td-agent" do
+  action [:enable, :start]
+end
