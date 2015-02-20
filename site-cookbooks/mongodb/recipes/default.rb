570894677cf913920255fa8d90dc3cf0ba60f2aa
hiroshi shimoda <hilolih@gmail.com>
2015-02-20 18:56:53 +0900

    add recipe mongodb

--- site-cookbooks/mongodb/recipes/default.rb
+++ site-cookbooks/mongodb/recipes/default.rb
@@ -6,3 +6,27 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cookbook_file "mongodb.repo" do
+  mode 0644
+  owner "root"
+  group "root"
+  path "/etc/yum.repos.d/mongodb.repo"
+  action :create_if_missing
+end
+
+%w{
+mongodb-org
+mongodb-org-mongos
+mongodb-org-server
+mongodb-org-shell
+mongodb-org-tools
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+service "mongod" do
+  action [:enable, :start]
+end
