0aa509d9bfdd814863d277e40b58d9b837403ad7
hiroshi shimoda <hilolih@gmail.com>
2015-03-03 18:59:19 +0900

    modified:   nodes/v1.json 	new file:   site-cookbooks/elasticsearch/files/default/elasticsearch.repo 	modified:   site-cookbooks/elasticsearch/recipes/default.rb 	modified:   site-cookbooks/fluentd/recipes/default.rb

--- site-cookbooks/elasticsearch/recipes/default.rb
+++ site-cookbooks/elasticsearch/recipes/default.rb
@@ -6,3 +6,39 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cookbook_file "elasticsearch.repo" do
+  mode 0644
+  owner "root"
+  group "root"
+  path "/etc/yum.repos.d/elasticsearch.repo"
+  action :create_if_missing
+end
+
+%w{
+elasticsearch
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+service "elasticsearch" do
+  action [:enable, :start]
+end
+
+%w{
+  gcc
+  libcurl-devel
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+elas_gem = "fluent-plugin-elasticsearch"
+bash "install fluent gem" do
+  code <<-"EOL"
+    /usr/lib64/fluent/ruby/bin/fluent-gem install #{elas_gem}
+  EOL
+  not_if "/usr/lib64/fluent/ruby/bin/fluent-gem list --local | grep #{elas_gem}"
+end
--- site-cookbooks/fluentd/recipes/default.rb
+++ site-cookbooks/fluentd/recipes/default.rb
@@ -32,3 +32,4 @@ end
 service "td-agent" do
   action [:enable, :start]
 end
+
